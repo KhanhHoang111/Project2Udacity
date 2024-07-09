2cc7efff12be8cb8e9645dc9e2705387606f27c7
KhanhHoang111 <43487666+KhanhHoang111@users.noreply.github.com>
2024-07-09 11:06:02 +0700

    Update __init__.py

--- Building-and-deploying-cloud-native-applications-master/NeighborlyAPI/getPosts/__init__.py
+++ Building-and-deploying-cloud-native-applications-master/NeighborlyAPI/getPosts/__init__.py
@@ -13,7 +13,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         url = os.environ["mongoDBConnection"]  # TODO: Update with appropriate MongoDB connection information
         client = pymongo.MongoClient(url)
-        database = client['nfmongo']
+        database = client['mymongodb1']
         collection = database['posts']
 
         result = collection.find({})
